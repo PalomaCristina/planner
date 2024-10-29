a3abddccdfb043e8696a53b2f69b13e9132870f1
Paloma Cristina <paloma1899@gmail.com>
2024-10-29 17:12:14 -0300

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export default function Index(){
+    return (
+        <View
+            style={{flex: 1, backgroundColor: "gray",  justifyContent: "center" }}>
+            <Text>Bem Vindo</Text>
+        </View>
+    )
+}
