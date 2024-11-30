537580b6ba3afd65e8ea89dc177295ca2e3815c4
Paloma Cristina <paloma1899@gmail.com>
2024-11-30 17:12:32 -0300

    include the touchpadOpacity and Calendar Icon

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,8 @@
 import { View, Text, Image } from "react-native";
 import { Input } from "@/components/input";
-import { MapPin } from "lucide-react-native";
+import { MapPin, Calendar as  CalendarIcon} from "lucide-react-native";
 import { colors } from "@/styles/colors";
+
 export default function Index(){
     return (
         <View className="flex-1 items-center justify-center px-5">
@@ -16,6 +17,11 @@ export default function Index(){
         <View className="w-full bg-zin-900 p-4 rounded-xl my-8 border border-zinc-800">
             <Input>
             <MapPin color={colors.zinc[400]} size={20} />
+            <Input.Field placeholder="Para onde?"/>
+            </Input>
+            <View className="border-b py-3 border-zinc-800"></View>
+            <Input>
+            <CalendarIcon color={colors.zinc[400]} size={20} />
             <Input.Field placeholder="Para onde?"/>
             </Input>
         </View>
---
+++ src/components/button.tsx
@@ -0,0 +1,15 @@
+import { Text, TouchableOpacity } from "react-native";
+
+type Variants = "primary" | "secondary";
+
+type ButtonPropos = '';
+function Button(){
+    return <TouchableOpacity></TouchableOpacity>
+
+}
+
+function Title(){
+    return <Text></Text>
+}
+
+Button.Title = Title
