37ba534f828c777fd4d19483283084b8fb36d542
Paloma <paloma.correa@dnkinfotelecom.com.br>
2025-02-06 18:49:31 -0300

    feat: create button "alterar" and "continuar". Create rule for colors secondary and primary of Button component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,34 +1,60 @@
 import { View, Text, Image } from "react-native";
 import { Input } from "@/components/input";
-import { MapPin, Calendar as  CalendarIcon} from "lucide-react-native";
+import {
+  MapPin,
+  Calendar as CalendarIcon,
+  Settings2,
+  UserRoundPlus,
+  ArrowRight,
+} from "lucide-react-native";
 import { colors } from "@/styles/colors";
 import { Button } from "@/components/button";
 
-export default function Index(){
-    return (
-        <View className="flex-1 items-center justify-center px-5">
-        <Image 
-        source={require("@/assets/logo.png")} 
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center px-5">
+      <Image
+        source={require("@/assets/logo.png")}
         className="h-8"
         resizeMode="contain"
-        />
-        <Text className="text-zinc-400 font-regular text-center text-lg mt-3">
-            Convide seus amigos e planeje {"\n"} sua próxima viagem!
+      />
+      <Text className="text-zinc-400 font-regular text-center text-lg mt-3">
+        Planeje suas tarefas
+      </Text>
+      <View className="w-full bg-zin-900 p-4 rounded-xl my-8 border border-zinc-800">
+        <Input>
+          <MapPin color={colors.zinc[400]} size={20} />
+          <Input.Field placeholder="Para onde?" />
+        </Input>
+
+        <Input>
+          <CalendarIcon color={colors.zinc[400]} size={20} />
+          <Input.Field placeholder="Quando?" />
+        </Input>
+
+        <View className="border-b py-3 border-zinc-800">
+          <Button variant="secondary">
+            <Button.Title>Alterar</Button.Title>
+            <Settings2 color={colors.zinc[200]} size={20}></Settings2>
+          </Button>
+        </View>
+
+        <Input>
+          <UserRoundPlus color={colors.zinc[400]} size={20} />
+          <Input.Field placeholder="Quais alunos?" />
+        </Input>
+
+        <Button>
+            <Button.Title>Continuar</Button.Title>
+            <ArrowRight color={colors.lime[950]} size={20}/>
+        </Button>
+      </View>
+        <Text className="text-zinc-500 font-regular text-center text-base">
+            Ao continuar utilizando o FIAP Planner, você automaticamente concorda com nossos{" "}
+            <Text className="text-zinc-200 underline">
+                termos de uso e politicas de privacidade.
+            </Text>
         </Text>
-        <View className="w-full bg-zin-900 p-4 rounded-xl my-8 border border-zinc-800">
-            <Input>
-            <MapPin color={colors.zinc[400]} size={20} />
-            <Input.Field placeholder="Para onde?"/>
-            </Input>
-            <Input>
-            <CalendarIcon color={colors.zinc[400]} size={20} />
-            <Input.Field placeholder="Quando?"/>
-            
-            <View className="border-b py-3 border-zinc-800">
-                <Button.Title>Alterar local/data</Button.Title>
-            </View>
-            </Input>
-        </View>
-        </View>
-)
+    </View>
+  );
 }
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,4 +1,6 @@
-import { Text, TextProps, TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { createContext, useContext } from "react";
+import { ActivityIndicator, Text, TextProps, TouchableOpacity, TouchableOpacityProps } from "react-native";
+import { clsx } from "clsx";
 
 type Variants = "primary" | "secondary";
 
@@ -6,13 +8,34 @@ type ButtonPropos = TouchableOpacityProps & {//essa variavel está recebendo tod
     variant?: Variants
     isLoading?: boolean
 }
-function Button({variant = "primary", isLoading, children, ...rest} : ButtonPropos){
-    return <TouchableOpacity {...rest}>{children}</TouchableOpacity>
 
-}
+const ThemeContext = createContext<{variant?: Variants}>({})
+function Button({variant = "primary", isLoading, children, ...rest} : ButtonPropos){
+    return <TouchableOpacity className={
+        clsx(
+            "w-full h-11 flex-row items-center justify-center rounded-lg gap-2",
+            {                
+                "bg-lime-300" : variant === "primary",
+                "bg-zinc-800" : variant === "secondary",
+            }
+        )}
+    activeOpacity={0.7}
+    disabled={isLoading}
+    {...rest}
+    >
+    <ThemeContext.Provider value={{variant}}>
+        { isLoading ? <ActivityIndicator/> : children}
+    </ThemeContext.Provider>    
+    </TouchableOpacity>
+
+}   
 
 function Title({ children} : TextProps){
-    return <Text>{children}</Text>
+    const { variant } = useContext(ThemeContext);
+    return <Text className={clsx("text-base font-semibold", {
+        "text-lime-950" : variant === "primary", 
+        "text-zinc-200" : variant === "secondary"
+    })}>{children}</Text>
 }
 
 Button.Title = Title
