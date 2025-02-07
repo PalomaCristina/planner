15772911cb1aa16f423f008b57e4e51b5d63beb4
Paloma <paloma.correa@dnkinfotelecom.com.br>
2025-02-07 09:39:51 -0300

    feat: include activity component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,11 +6,25 @@ import {
   Settings2,
   UserRoundPlus,
   ArrowRight,
+  ClipboardList,
 } from "lucide-react-native";
 import { colors } from "@/styles/colors";
 import { Button } from "@/components/button";
+import { useState } from "react";
+
+enum StepForm {
+  TASK_DETAILS = 1,
+  ADD_EMAIL = 2,
+}
 
 export default function Index() {
+  const [stepForm, setStepForm] = useState(StepForm.TASK_DETAILS);
+
+  function handleStepNextForm() {
+    if (stepForm === StepForm.TASK_DETAILS) {
+      return setStepForm(StepForm.ADD_EMAIL);
+    }
+  }
   return (
     <View className="flex-1 items-center justify-center px-5">
       <Image
@@ -18,43 +32,58 @@ export default function Index() {
         className="h-8"
         resizeMode="contain"
       />
+      <Image source={require('@/assets/bg.png')} className="absolute"></Image>
       <Text className="text-zinc-400 font-regular text-center text-lg mt-3">
         Planeje suas tarefas
       </Text>
       <View className="w-full bg-zin-900 p-4 rounded-xl my-8 border border-zinc-800">
         <Input>
-          <MapPin color={colors.zinc[400]} size={20} />
-          <Input.Field placeholder="Para onde?" />
+          <ClipboardList color={colors.zinc[400]} size={20} />
+          <Input.Field placeholder="O que fazer?" editable={stepForm === StepForm.TASK_DETAILS} />
         </Input>
 
         <Input>
           <CalendarIcon color={colors.zinc[400]} size={20} />
-          <Input.Field placeholder="Quando?" />
+          <Input.Field placeholder="Quando?" editable={stepForm === StepForm.TASK_DETAILS}/>
         </Input>
+        
+        {stepForm === StepForm.ADD_EMAIL && (
+          <>
+            <View className="border-b py-3 border-zinc-800">
+              <Button
+                variant="secondary"
+                onPress={() => {
+                  setStepForm(StepForm.TASK_DETAILS);
+                }}
+              >
+                <Button.Title>Alterar</Button.Title>
+                <Settings2 color={colors.zinc[200]} size={20}></Settings2>
+              </Button>
+            </View>
 
-        <View className="border-b py-3 border-zinc-800">
-          <Button variant="secondary">
-            <Button.Title>Alterar</Button.Title>
-            <Settings2 color={colors.zinc[200]} size={20}></Settings2>
-          </Button>
-        </View>
-
-        <Input>
-          <UserRoundPlus color={colors.zinc[400]} size={20} />
-          <Input.Field placeholder="Quais alunos?" />
-        </Input>
-
-        <Button>
-            <Button.Title>Continuar</Button.Title>
-            <ArrowRight color={colors.lime[950]} size={20}/>
+            <Input>
+              <UserRoundPlus color={colors.zinc[400]} size={20} />
+              <Input.Field placeholder="Convidar colaborador" />
+            </Input>
+          </>
+        )}
+        <Button onPress={handleStepNextForm}>
+          <Button.Title>
+            {
+            stepForm === StepForm.TASK_DETAILS 
+            ? "Continuar"
+            : "Confirmar Tarefa"
+            }</Button.Title>
+          <ArrowRight color={colors.lime[950]} size={20} />
         </Button>
       </View>
-        <Text className="text-zinc-500 font-regular text-center text-base">
-            Ao continuar utilizando o FIAP Planner, você automaticamente concorda com nossos{" "}
-            <Text className="text-zinc-200 underline">
-                termos de uso e politicas de privacidade.
-            </Text>
+      <Text className="text-zinc-500 font-regular text-center text-base">
+        Ao continuar utilizando o FIAP Planner, você automaticamente concorda
+        com nossos{" "}
+        <Text className="text-zinc-200 underline">
+          termos de uso e politicas de privacidade.
         </Text>
+      </Text>
     </View>
   );
 }
